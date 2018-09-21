298e4bdb3cfe834ae4a1d6dd7c2e53aa325377ed
Märt Kalmo <mart.kalmo@gmail.com>
2018-09-21 11:48:06 +0300

    added test for hw4

--- src/main/java/tests/Hw4.java
+++ src/main/java/tests/Hw4.java
@@ -1,10 +1,14 @@
 package tests;
 
 import org.junit.Test;
+import tests.model.Order;
 import util.PenaltyOnTestFailure;
 
-import static org.junit.Assert.fail;
+import java.util.List;
+import java.util.stream.Collectors;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 public class Hw4 extends AbstractHw {
 
@@ -12,8 +16,28 @@ public class Hw4 extends AbstractHw {
 
     @Test
     @PenaltyOnTestFailure(10)
-    public void notImplementedYet() {
-        fail("Not implemented yet");
+    public void baseUrlResponds() {
+        boolean isSuccess = sendRequest(getBaseUrl());
+
+        assertThat(isSuccess, is(true));
+    }
+
+    @Test
+    @PenaltyOnTestFailure(10)
+    public void canGetListOfAllOrders() {
+
+        postOrder("api/orders", new Order("A123"));
+        postOrder("api/orders", new Order("A456"));
+
+        List<Order> allOrders = getList("api/orders");
+
+        assertThat(allOrders.size(), is(greaterThanOrEqualTo(2)));
+
+        List<String> allNumbers = allOrders.stream()
+                .map(o -> o.getOrderNumber())
+                .collect(Collectors.toList());
+
+        assertThat(allNumbers, hasItems("A123", "A456"));
     }
 
     @Override
