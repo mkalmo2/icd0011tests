687b0684026d80f10a4b532f0ff5528f2b66a904
Märt Kalmo <mart.kalmo@gmail.com>
2019-11-01 09:58:15 +0200

    added hw8

--- src/test/java/tests/Hw8.java
+++ src/test/java/tests/Hw8.java
@@ -1,16 +1,73 @@
 package tests;
 
 import org.junit.Test;
-import util.PenaltyOnTestFailure;
+import tests.model.Order;
+import tests.model.OrderRow;
+import util.JavaFileReader;
 
-import static org.junit.Assert.fail;
+import java.util.List;
 
-public class Hw8 {
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class Hw8 extends AbstractHw {
+
+    private final String baseUrl = "http://localhost:8080/";
+
+    private final String pathToProjectSourceCode = "";
 
     @Test
-    @PenaltyOnTestFailure(10)
-    public void missing() {
-        fail("not ready yet");
+    public void addsOrderWithOrderRows() {
+        delete("api/orders");
+
+        postOrder("api/orders", getOrder("A123", "CPU", "Motherboard"));
+        postOrder("api/orders", getOrder("A123", "Mouse"));
+        postOrder("api/orders", getOrder("A123", "Monitor", "Printer"));
+
+        List<Order> orders = getList("api/orders");
+
+        assertThat(orders.size(), is(3));
+        assertThat(orders.get(2).getOrderRows().size(), is(2));
+        assertThat(orders.get(2).getOrderRows().get(1).getItemName(), is("Printer"));
+    }
+
+    @Test
+    public void shouldNotUseSpringBoot() {
+        assertProjectSourcePathIsSet(pathToProjectSourceCode);
+
+        List<JavaFileReader.File> sourceCode = getProjectSource(pathToProjectSourceCode);
+
+        assertDoesNotContainString(sourceCode, "SpringApplication");
+        assertDoesNotContainString(sourceCode, "springframework.boot");
+    }
+
+    @Test
+    public void shouldUseOnlyJpaForDatabaseAccess() {
+        assertProjectSourcePathIsSet(pathToProjectSourceCode);
+
+        List<JavaFileReader.File> sourceCode = getProjectSource(pathToProjectSourceCode);
+
+        assertDoesNotContainString(sourceCode, "getConnection");
+        assertDoesNotContainString(sourceCode, "createStatement");
+        assertDoesNotContainString(sourceCode, "prepareStatement");
+        assertDoesNotContainString(sourceCode, "executeQuery");
+        assertDoesNotContainString(sourceCode, "getGeneratedKeys");
+
+        assertDoesNotContainString(sourceCode, "JdbcTemplate");
+        assertDoesNotContainString(sourceCode, "SimpleJdbcInsert");
+    }
+
+    private Order getOrder(String number, String ... items) {
+        Order order = new Order(number);
+        for (String item : items) {
+            order.add(new OrderRow(item, 1, 100));
+        }
+        return order;
+    }
+
+    @Override
+    protected String getBaseUrl() {
+        return baseUrl;
     }
 
 }
