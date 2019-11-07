da36ed2ef9a03aae3d259c18475d2473764d9903
Märt Kalmo <mart.kalmo@gmail.com>
2019-11-07 10:27:45 +0200

    updated test for hw8

--- src/test/java/tests/Hw8.java
+++ src/test/java/tests/Hw8.java
@@ -6,9 +6,10 @@ import tests.model.OrderRow;
 import util.JavaFileReader;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
 
 public class Hw8 extends AbstractHw {
 
@@ -18,17 +19,25 @@ public class Hw8 extends AbstractHw {
 
     @Test
     public void addsOrderWithOrderRows() {
-        delete("api/orders");
 
-        postOrder("api/orders", getOrder("A123", "CPU", "Motherboard"));
-        postOrder("api/orders", getOrder("A123", "Mouse"));
-        postOrder("api/orders", getOrder("A123", "Monitor", "Printer"));
+        String id1 = postOrder("api/orders", "A123", "CPU", "Motherboard");
+        String id2 = postOrder("api/orders", "A456", "Mouse");
+        String id3 = postOrder("api/orders", "A789", "Monitor", "Printer");
 
-        List<Order> orders = getList("api/orders");
+        List<Order> orderList = getList("api/orders");
 
-        assertThat(orders.size(), is(3));
-        assertThat(orders.get(2).getOrderRows().size(), is(2));
-        assertThat(orders.get(2).getOrderRows().get(1).getItemName(), is("Printer"));
+        assertHasIds(orderList, id1, id2, id3);
+
+        assertContainsItems(orderList, id1, "CPU", "Motherboard");
+        assertContainsItems(orderList, id2, "Mouse");
+        assertContainsItems(orderList, id3, "Monitor", "Printer");
+    }
+
+    private String postOrder(String url, String number, String ... items) {
+        return postOrder(url, getOrder(number, items))
+                .getValue()
+                .getId();
+
     }
 
     @Test
@@ -68,6 +77,29 @@ public class Hw8 extends AbstractHw {
     @Override
     protected String getBaseUrl() {
         return baseUrl;
+    }
+
+    private void assertContainsItems(
+            List<Order> orderList, String id, String ... itemName) {
+
+        Order order = orderList.stream()
+                .filter(o -> o.getId().equals(id))
+                .findFirst()
+                .get();
+
+        List<String> items = order.getOrderRows().stream()
+                .map(or -> or.getItemName())
+                .collect(Collectors.toList());
+
+        assertThat(items, contains(itemName));
+    }
+
+    private void assertHasIds(List<Order> orderList, String ... ids) {
+        List<String> returnedOrderIds = orderList.stream()
+                .map(order -> order.getId())
+                .collect(Collectors.toList());
+
+        assertThat(returnedOrderIds, hasItems(ids));
     }
 
 }
