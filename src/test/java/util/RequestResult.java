022a276b153578eb2c5f9616918b2aa2e4071ac0
Märt Kalmo <mart.kalmo@gmail.com>
2019-11-17 12:01:01 +0200

    added tests for hw9

--- src/test/java/util/RequestResult.java
+++ src/test/java/util/RequestResult.java
@@ -1,12 +1,19 @@
 package util;
 
-import javax.ws.rs.core.Cookie;
 import java.util.Optional;
 
 public class RequestResult {
 
     private Integer statusCode;
-    private Optional<Cookie> cookie = Optional.empty();
+    private Optional<String> authorization = Optional.empty();
+
+    public Optional<String> getAuthorization() {
+        return authorization;
+    }
+
+    public void setAuthorization(Optional<String> authorization) {
+        this.authorization = authorization;
+    }
 
     public Integer getStatusCode() {
         return statusCode;
@@ -14,13 +21,5 @@ public class RequestResult {
 
     public void setStatusCode(Integer statusCode) {
         this.statusCode = statusCode;
-    }
-
-    public Optional<Cookie> getCookie() {
-        return cookie;
-    }
-
-    public void setCookie(Optional<Cookie> cookie) {
-        this.cookie = cookie;
     }
 }
