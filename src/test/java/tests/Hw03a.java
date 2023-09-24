b71ef9f911f124d4c724eb08f9d87954d7f5eb9b
Märt Kalmo <mart.kalmo@gmail.com>
2023-09-24 13:00:48 +0300

    hw03 test fix

--- src/test/java/tests/Hw03a.java
+++ src/test/java/tests/Hw03a.java
@@ -18,7 +18,7 @@ public class Hw03a extends AbstractHw {
 
     @Test
     public void parserCanHandleMoreComplexJson() {
-        JsonValue data = new SampleDataProvider(3).getRandomData();
+        JsonValue data = new SampleDataProvider(5).getRandomData();
 
         Map<String, Object> inputMap = toObjectMap(data);
 
