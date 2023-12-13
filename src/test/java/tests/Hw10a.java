b05ba75acb9926f29339a33b86421c4d7e5f6372
Märt Kalmo <mart.kalmo@gmail.com>
2023-12-13 16:07:53 +0200

    added test for hw10a

---
+++ src/test/java/tests/Hw10a.java
@@ -0,0 +1,44 @@
+package tests;
+
+import org.junit.Test;
+import util.FileFinder;
+
+import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.*;
+
+public class Hw10a extends Hw10 {
+
+    private final String baseUrl = "http://localhost:8080/";
+
+    private final String pathToProjectSourceCode = "";
+
+    @Test
+    public void repositoryShouldNotContainJavaFiles() {
+        assumeProjectSourcePathIsSet(pathToProjectSourceCode);
+
+        Path sourcePath = resolveProjectSourcePath(pathToProjectSourceCode);
+
+        String fileList = new FileFinder()
+                .getAllFilesFrom(sourcePath, List.of(".java"))
+                .stream()
+                .map(path -> path.getFileName().toString())
+                .collect(Collectors.joining(", "));
+
+        if (fileList.isEmpty()) {
+            return;
+        }
+
+        String message = "Project repository should not contain Java files" +
+                " but there are: " + fileList;
+
+        fail(message);
+    }
+
+    @Override
+    protected String getBaseUrl() {
+        return baseUrl;
+    }
+}
