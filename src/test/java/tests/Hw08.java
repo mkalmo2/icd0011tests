9361c3ee3a06fdb39bbe0088b5dd269283a98361
Märt Kalmo <mart.kalmo@gmail.com>
2025-10-13 10:38:11 +0300

    added tests for Hw08

---
+++ src/test/java/tests/Hw08.java
@@ -0,0 +1,68 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+import tests.model.Order;
+import tests.model.OrderRow;
+import tests.model.Result;
+import util.FileReader;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Hw08 extends AbstractHw {
+
+    private final String baseUrl = "http://localhost:8080/";
+
+    private final String pathToProjectSourceCode = "";
+
+    @Test
+    public void getOrderUrlIsChanged() {
+        Result<Order> result = postOrder("api/orders", new Order("A123"));
+
+        String idOfPostedOrder = result.getValue().getId();
+
+        Order order = getOne("api/orders/" + idOfPostedOrder);
+
+        assertThat(order.getOrderNumber()).isEqualTo("A123");
+        assertThat(order.getId()).isEqualTo(idOfPostedOrder);
+    }
+
+    @Test
+    public void returnsErrorsWhenOrderRowsAreNotValid() {
+        Order order = new Order("A1");
+        order.add(new OrderRow("Motherboard", 0, 5));
+        order.add(new OrderRow("Cpu", 5, 0));
+
+        Result<Order> result = postOrder("api/orders", order);
+
+        assertThat(result.isSuccess()).isFalse();
+        assertThat(result.getErrors()).hasSize(2);
+    }
+
+    @Test
+    public void shouldUseSpringMvcInsteadOfLowerLevelClasses() {
+        assumeProjectSourcePathIsSet(pathToProjectSourceCode);
+
+        List<FileReader.File> sourceCode = getProjectSource(pathToProjectSourceCode);
+
+        assertDoesNotContainString(sourceCode, "HttpServlet");
+        assertDoesNotContainString(sourceCode, "@WebListener");
+        assertDoesNotContainString(sourceCode, "@WebServlet");
+    }
+
+    @Test
+    public void shouldNotUseSpringBootYet() {
+        assumeProjectSourcePathIsSet(pathToProjectSourceCode);
+
+        List<FileReader.File> sourceCode = getProjectSource(pathToProjectSourceCode);
+
+        assertDoesNotContainString(sourceCode, "SpringApplication");
+        assertDoesNotContainString(sourceCode, "springframework.boot");
+    }
+
+    @Override
+    protected String getBaseUrl() {
+        return baseUrl;
+    }
+}
